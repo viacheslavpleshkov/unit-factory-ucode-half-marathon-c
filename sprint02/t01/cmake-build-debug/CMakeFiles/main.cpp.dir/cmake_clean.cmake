e6ed2dcf98731a1dd2722286613d5cdb1d41f09e
Slava Pleshkov <mail@slavapleshkov.com>
2020-08-23 23:08:39 +0300

    Update files

---
+++ sprint02/t01/cmake-build-debug/CMakeFiles/main.cpp.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.cpp.dir/src/createFile.cpp.o"
+  "CMakeFiles/main.cpp.dir/src/errors.cpp.o"
+  "CMakeFiles/main.cpp.dir/src/parsFile.cpp.o"
+  "main.cpp"
+  "main.cpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ sprint02/t01/src/createFile.cpp
@@ -0,0 +1,14 @@
+#include "countUniqueWords.h"
+
+void createFile(std::string file_name, std::multiset<std::string> names) {
+    std::string temp;
+    std::stringstream stringstream;
+
+    stringstream << file_name;
+    getline(stringstream, temp, '.');
+    std::ofstream newfile(temp + "_mod.txt");
+
+    for (auto item : names)
+        newfile << item << std::endl;
+    newfile.close();
+}
