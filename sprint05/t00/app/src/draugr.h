7b1b3ef1aa91c705577ead9afc26626818bd7fdf
Slava Pleshkov <mail@slavapleshkov.com>
2020-08-27 20:24:56 +0300

    Update files

---
+++ sprint05/t00/app/src/draugr.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <regex>
+
+class Draugr {
+public:
+    Draugr();
+
+    void shoutPhrase(int shoutNumber) const;
+
+private:
+    double m_health;
+    const int m_frostResist;
+};
+
+#endif //DRAUGR_H_
