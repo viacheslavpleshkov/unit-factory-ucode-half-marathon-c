12c3a8725568d73a4986f23817d4c5e2c6b1903c
Slava Pleshkov <mail@slavapleshkov.com>
2020-08-28 22:24:35 +0300

    Update files

---
+++ sprint06/t00/build/app/CMakeFiles/mightyWizard.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mightyWizard.dir/main.cpp.o"
+  "mightyWizard"
+  "mightyWizard.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mightyWizard.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
