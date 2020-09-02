76a6f7da800c5842890dd09c849cb9e1e144b3e4
Viacheslav Pleshkov <mail@viacheslavpleshkov.com>
2020-09-02 19:45:33 +0300

    Add t01

--- OOP/t01/accessModifiers.h
+++ OOP/t01/accessModifiers.h
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-class A {
+class Class {
 public:
     int a = 1;
 protected:
@@ -16,9 +16,27 @@ private:
 public:
 };
 
-class B : public A {
+class subClass : public Class {
 public:
     void print() {
-        cout << a << b << endl;
+        cout << this->a << this->b << endl;
+    }
+};
+
+
+class Device {
+public:
+    int serial_number = 12345678;
+
+    void turn_on() {
+        cout << "Device is on" << endl;
+    }
+};
+
+class Computer: private Device {
+public:
+    void say_hello() {
+        turn_on();
+        cout << "Welcome to Windows 95!" << endl;
     }
 };
